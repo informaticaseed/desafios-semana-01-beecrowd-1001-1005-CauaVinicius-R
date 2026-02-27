deadc59902622fc5432d0330ecace441b78bd3fd
CauaVinicius-R <nobrumeteocapa@gmail.com>
2026-02-27 10:11:33 -0300

    Update desafio_1003.py

--- desafio_1003.py
+++ desafio_1003.py
@@ -15,3 +15,12 @@ de A e B.
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1003
 
 # Escreva sua solução abaixo
+# Lê os dois valores inteiros
+A = int(input())
+B = int(input())
+
+# Realiza a operação solicitada
+SOMA = A + B
+
+# Exibe o resultado com a formatação exata
+print(f"SOMA = {SOMA}")
